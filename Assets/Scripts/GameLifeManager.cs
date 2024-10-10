3f1960a1aad488ccb91d85c853954a7a6753ecb8
Lewis2226 <131185164+Lewis2226@users.noreply.github.com>
2024-10-09 21:42:31 -0600

    pruebas de array

--- Assets/Scripts/GameLifeManager.cs
+++ Assets/Scripts/GameLifeManager.cs
@@ -6,13 +6,19 @@ public class GameLifeManager : MonoBehaviour
 {
     public Color AliveColor;
     public Color DeadColor;
-    public SpriteRenderer render;
+    public int cellNum;
+    //public SpriteRenderer render;
     public bool test;
+    public string[] totalCells; 
     void Start()
     {
-        
+        for (int i = 0; i < cellNum; i++)
+        {
+            totalCells[i] = $"la celula {i}, esta muerta";
+            Debug.Log(totalCells[i]);
+        }
     }
-
+    /*
     public void ChangeColor(bool state)
     {
         if(state)
@@ -25,9 +31,10 @@ public class GameLifeManager : MonoBehaviour
             render.color= DeadColor;
         }
     }
+    */
 
     void Update()
     {
-        ChangeColor(test);
+        //ChangeColor(test);
     }
 }
