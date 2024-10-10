b731ccb538b936bc9547696e37fabd341f9ee065
Lewis2226 <131185164+Lewis2226@users.noreply.github.com>
2024-10-09 21:58:23 -0600

    a

--- Assets/Scripts/GameLifeManager.cs
+++ Assets/Scripts/GameLifeManager.cs
@@ -9,14 +9,10 @@ public class GameLifeManager : MonoBehaviour
     public int cellNum;
     //public SpriteRenderer render;
     public bool test;
-    public string[] totalCells; 
+    Hashtable totalCells; 
     void Start()
     {
-        for (int i = 0; i < cellNum; i++)
-        {
-            totalCells[i] = $"la celula {i}, esta muerta";
-            Debug.Log(totalCells[i]);
-        }
+        LifeGiver();
     }
     /*
     public void ChangeColor(bool state)
@@ -36,5 +32,30 @@ public class GameLifeManager : MonoBehaviour
     void Update()
     {
         //ChangeColor(test);
+        LifeCheck();
+    }
+
+    void LifeGiver()
+    {
+        int rnd; 
+        for(int i = 0; i < cellNum; i++) 
+        {
+            rnd = Random.Range(0, 2);
+            if (rnd == 0)
+            {
+                totalCells.Add(i, "Celula esta muerta");   
+            }
+            else
+            {
+                totalCells.Add(i, "Celula  esta viva");
+            }
+            Debug.Log(totalCells[i].ToString());
+
+        }
+    }
+
+    void LifeCheck()
+    {
+
     }
 }
